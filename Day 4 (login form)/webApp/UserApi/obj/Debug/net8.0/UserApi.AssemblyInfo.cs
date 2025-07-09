3a27d273d948040b2dd082f4c36bd5c856f90788
dhruvil-17 <dhruvildodia91@gmail.com>
2025-07-09 10:06:15 +0530

    Clean push: login-frontend fixed day-4

--- Day 4 (login form)/webApp/UserApi/obj/Debug/net8.0/UserApi.AssemblyInfo.cs
+++ Day 4 (login form)/webApp/UserApi/obj/Debug/net8.0/UserApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+389b0509480c9bd54d039d2bc26a05501df6e1d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
