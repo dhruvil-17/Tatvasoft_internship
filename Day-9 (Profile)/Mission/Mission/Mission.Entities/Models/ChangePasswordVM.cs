a07210c8600266020063b20f17234965196384c2
dhruvil-17 <dhruvildodia91@gmail.com>
2025-07-15 15:58:15 +0530

    day 9 work

---
+++ Day-9 (Profile)/Mission/Mission/Mission.Entities/Models/ChangePasswordVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission.Entities.Models
+{
+    internal class ChangePasswordVM
+    {
+        public int UserId { get; set; }
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
